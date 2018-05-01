62d44a2d03c8842ce088c81474304a8058a5d8ee
andre303 <157andre>
2018-05-01 16:51:37 +0300

    Initial commit

---
+++ src/main/java/views/FXML_MainFormController.java
@@ -0,0 +1,18 @@
+package views;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class FXML_MainFormController implements Initializable {
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+}
