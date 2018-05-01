2cdc227155674e24959225bc539dd90ac6bb8dc4
kopervas <sergkopervas@gmail.com>
2018-05-01 17:29:39 +0300

    Correct the path to the file "FXML_MainForm.fxml"

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -9,8 +9,9 @@ public class MainClass  extends Application {
         launch(args);
 
     }
+
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/FXML_MainForm.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("FXML_MainForm.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.setTitle("ChannelCompare");
